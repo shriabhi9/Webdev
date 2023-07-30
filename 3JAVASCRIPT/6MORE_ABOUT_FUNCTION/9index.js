7ae9ef5a585496f79d3744a361d6ea781d7634bf
Abhishek Shrivastava <shriabhishek929@gmail.com>
2023-07-30 22:17:32 +0530

    js

--- 3JAVASCRIPT/6MORE_ABOUT_FUNCTION/9index.js
+++ 3JAVASCRIPT/6MORE_ABOUT_FUNCTION/9index.js
@@ -5,8 +5,28 @@
 
 // Global variable is nothing but the varibale that assign out of the function
 
-var x = 3 // it will be considere as global variable because it is out of the function
-function showNumber(){
-    console.log(x)
+// var x = 3 // global variable --> accessable at anywhere
+// function showNumber(){
+//     var y = 5 // local variable -> not accessable outside the fun
+//     console.log(y)
+//     console.log(x)
+// }
+// console.log(y) // error
+
+// function f1(){
+//     console.log(x)
+// }
+// f1()
+// showNumber()
+
+// scope
+// basically variable declared with let and const have local scope
+// and variable declared with var has an gloable scope it is avalible at anywhere
+/// whether i declared it anywhere
+
+{
+    // let a = 3;
+    var b = 5; 
 }
-console.log(x)
+// console.log(a)
+console.log(b)
